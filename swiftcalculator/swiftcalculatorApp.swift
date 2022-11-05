10c8fd133a04182e7022929115a6012b1628ec85
bahittinn <bahittin84@gmail.com>
2022-11-05 23:35:39 +0300

    Initial Commit

---
+++ swiftcalculator/swiftcalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftcalculatorApp.swift
+//  swiftcalculator
+//
+//  Created by akdeniz bilgisayar on 5.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftcalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
